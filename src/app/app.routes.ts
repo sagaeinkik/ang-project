16866150da05b05d74df0c7c43b9720deac49e2b
Saga Einarsdotter Kikajon <saga@kikajon.se>
2024-07-14 22:14:04 +0200

    Gjort en halv header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: '**', redirectTo: '' },
+];
